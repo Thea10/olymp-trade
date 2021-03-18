1f65d6effa7c4a3181a1d4a4844c4a55f27168cc
Cynthia <olucynakin@gmail.com>
2021-03-18 22:33:23 +0100

    base components and images

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
+import Scroll from "./components/Scroll";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Scroll>
     <App />
+
+
+    </Scroll>
   </React.StrictMode>,
   document.getElementById('root')
 );
